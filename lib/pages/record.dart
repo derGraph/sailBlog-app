236f91151204ac705086bf7356747d2c015e30e5
derGraph <paulantongraf@gmail.com>
2025-03-05 18:00:48 +0100

    add simple Track

--- lib/pages/record.dart
+++ lib/pages/record.dart
@@ -9,6 +9,14 @@ import 'package:sailblog/database.dart';
 import 'package:sailblog/recorder.dart';
 import 'package:sailblog/server.dart';
 
+final tileProvider = FMTCTileProvider.allStores(
+  allStoresStrategy: BrowseStoreStrategy.readUpdateCreate,
+  loadingStrategy: BrowseLoadingStrategy.onlineFirst,
+);
+
+final mapController = MapController();
+bool mapReady = false;
+
 class RecordPage extends StatefulWidget {
   const RecordPage({super.key});
 
@@ -16,44 +24,75 @@ class RecordPage extends StatefulWidget {
   State<RecordPage> createState() => _RecordPage();
 }
 
+class RecordPageBGTask extends ChangeNotifier {
+  List<LatLng> points = [];
+  List<DatapointLocal> newDatapoints = [];
+  int newUploadable = 0;
+
+  void scaleMap() {
+      if(mapReady && points.isNotEmpty){
+        mapController.fitCamera(CameraFit.bounds(bounds: LatLngBounds.fromPoints(points), padding: EdgeInsets.all(50)));
+      }
+  }
+
+  Future<void> updateData(Timer timer) async {
+    newDatapoints = await database.getDatapoints();
+    newUploadable = await database.countUploadableDatapoints();
+
+    if(newDatapoints.length > points.length){
+      while(points.length < newDatapoints.length){
+        points.add(LatLng(newDatapoints[points.length].lat!.toDouble(), newDatapoints[points.length].long!.toDouble()));
+      }
+      scaleMap();
+    }
+
+    if (recorder.online) {
+      server.uploadDatapoints();
+    }
+    notifyListeners();
+  }
+}
+
+RecordPageBGTask bgTask = RecordPageBGTask();
+
 class _RecordPage extends State<RecordPage> {
-  final tileProvider = FMTCTileProvider.allStores(
-    allStoresStrategy: BrowseStoreStrategy.readUpdateCreate,
-    loadingStrategy: BrowseLoadingStrategy.onlineFirst,
+  FlutterMap map = FlutterMap(
+    mapController: mapController,
+    options: MapOptions(
+        initialCenter: LatLng(43.95, 14.79),
+        initialZoom: 8,
+        keepAlive: true,
+        onMapReady: () {
+          mapReady = true;
+          bgTask.scaleMap();
+        },
+        interactionOptions: InteractionOptions(
+            flags: InteractiveFlag.drag |
+                InteractiveFlag.doubleTapZoom |
+                InteractiveFlag.flingAnimation |
+                InteractiveFlag.pinchMove |
+                InteractiveFlag.pinchZoom |
+                InteractiveFlag.doubleTapDragZoom |
+                InteractiveFlag.scrollWheelZoom)),
+    children: [
+      TileLayer(
+        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        maxZoom: 19,
+        tileProvider: tileProvider,
+      ),
+      TileLayer(
+        urlTemplate: 'http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png',
+        maxZoom: 19,
+        minNativeZoom: 9,
+        tileProvider: tileProvider,
+      ),
+      SimpleAttributionWidget(source: Text("OpenStreetMap")),
+      TrackOverlay()
+    ],
   );
 
   @override
-  Widget build(BuildContext context) {
-    FlutterMap map = FlutterMap(
-      mapController: MapController(),
-      options: MapOptions(
-          initialCenter: LatLng(43.95, 14.79),
-          initialZoom: 8,
-          keepAlive: true,
-          interactionOptions: InteractionOptions(
-              flags: InteractiveFlag.drag |
-                  InteractiveFlag.doubleTapZoom |
-                  InteractiveFlag.flingAnimation |
-                  InteractiveFlag.pinchMove |
-                  InteractiveFlag.pinchZoom |
-                  InteractiveFlag.doubleTapDragZoom |
-                  InteractiveFlag.scrollWheelZoom)),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          maxZoom: 19,
-          tileProvider: tileProvider,
-        ),
-        TileLayer(
-          urlTemplate: 'http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png',
-          maxZoom: 19,
-          minNativeZoom: 9,
-          tileProvider: tileProvider,
-        ),
-        SimpleAttributionWidget(source: Text("OpenStreetMap"))
-      ],
-    );
-
+  Widget build(BuildContext context) {    
     recorder.setMode(recorder.mode);
     return Stack(children: [
       map,
@@ -64,6 +103,34 @@ class _RecordPage extends State<RecordPage> {
       ),
       StatusCard(),
     ]);
+  }
+}
+
+class TrackOverlay extends StatefulWidget {
+  const TrackOverlay({super.key});
+
+  @override
+  State<TrackOverlay> createState() => _TrackOverlayState();
+}
+
+class _TrackOverlayState extends State<TrackOverlay> {
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: bgTask,
+      builder: (BuildContext context, Widget? child) {
+        return PolylineLayer(
+          polylines: [
+            Polyline(
+              points: bgTask.points,
+              strokeWidth: 5,
+              useStrokeWidthInMeter: true,
+              color: Colors.blue,
+            ),
+          ],
+        );
+      }, 
+    );
   }
 }
 
@@ -123,14 +190,12 @@ class StatusCard extends StatefulWidget {
 
 class _StatusCardState extends State<StatusCard> {
   late Timer _timer;
-  List<DatapointLocal> _datapoints = [];
-  int _uploadableCount = 0;
   final DateFormat _timeFormat = DateFormat('HH:mm:ss');
 
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(const Duration(milliseconds: 100), _updateData);
+    _timer = Timer.periodic(const Duration(milliseconds: 100), bgTask.updateData);
   }
 
   @override
@@ -139,35 +204,18 @@ class _StatusCardState extends State<StatusCard> {
     super.dispose();
   }
 
-  Future<void> _updateData(Timer timer) async {
-    final newDatapoints = await database.getDatapoints();
-    final newUploadable = await database.countUploadableDatapoints();
-
-    if (recorder.online) {
-      server.uploadDatapoints();
-    }
-
-    if (mounted) {
-      setState(() {
-        _datapoints = newDatapoints;
-        _uploadableCount = newUploadable;
-      });
-    }
-  }
-
   String _getGpsStatus() {
-    if (_datapoints.isEmpty) return "waiting for GPS!";
+    if (bgTask.newDatapoints.isEmpty) return "waiting for GPS!";
 
-    final timeDiff = DateTime.now().difference(_datapoints.last.time!);
-    if (timeDiff.inSeconds < 30) return "GPS: ok!";
+    final timeDiff = DateTime.now().difference(bgTask.newDatapoints.last.time!);
     if (recorder.mode == Modes.off) return "GPS: not recording!";
+    if (timeDiff.inSeconds < 30) return "GPS: ok!";
     return "GPS: Waiting for GPS!";
   }
 
   @override
   Widget build(BuildContext context) {
-    final gpsStatus = _getGpsStatus();
-    final uploadedCount = _datapoints.length - _uploadableCount;
+    final uploadedCount = bgTask.newDatapoints.length - bgTask.newUploadable;
 
     return Align(
       alignment: Alignment.bottomCenter,
@@ -175,16 +223,21 @@ class _StatusCardState extends State<StatusCard> {
         margin: const EdgeInsets.all(32.0),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('uploaded $uploadedCount/${_datapoints.length} $gpsStatus'),
-              if (_datapoints.isNotEmpty)
-                Text(
-                    'last Datapoint ${_timeFormat.format(_datapoints.last.time!.toLocal())} '
-                    'accuracy: ${_datapoints.last.hAccuracy?.truncate(scale: 2)}m'),
-            ],
-          ),
+          child:
+              ListenableBuilder(
+                listenable: bgTask,
+                builder: (BuildContext context, Widget? child) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('uploaded $uploadedCount/${bgTask.newDatapoints.length} ${_getGpsStatus()}'),
+                      if (bgTask.newDatapoints.isNotEmpty)
+                        Text(
+                            'last Datapoint ${_timeFormat.format(bgTask.newDatapoints.last.time!.toLocal())} '
+                            'accuracy: ${bgTask.newDatapoints.last.hAccuracy?.truncate(scale: 2)}m'),
+                    ],
+                  );
+          }),
         ),
       ),
     );
