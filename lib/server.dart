7072267588a156fd7289ee0675bcfe10c0dd3685
derGraph <paulantongraf@gmail.com>
2026-07-10 02:10:46 +0200

    undo last commit

--- lib/server.dart
+++ lib/server.dart
@@ -169,7 +169,7 @@ class Server {
       // FIX 2: Fixed action routing mapping (?/login instead of ?/login=) 
       // FIX 3: Added Options setting explicit JSON Content-Type mapping
       response = await dio.post(
-        '/sign_in', 
+        '/sign_in?/login', 
         data: loginData,
         options: Options(contentType: Headers.jsonContentType),
       );
